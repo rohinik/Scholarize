d4991b7a8acea4bd6857ac96be6ff7f143e01997
Rohini Kulkarni <rohinik@thoughtworks.com>
2013-03-29 00:15:31 +0530

    My initial commit message

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scholarize::Application.config.secret_token = '83590a2b5c1d0aa313088c30c0152911df73378fbb32ff99a0ff71bdf233dd9fb79b3c1137a0aea6ae4d25b7639aa82674823d7e83847b37ee826e921b8b1f6c'
